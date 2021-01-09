ac8fa50668713c4971bbedb11ac9f054b1ddf0e6
Ernesto cardenas <ernesto.cardenas79@gmail.com>
2021-01-09 13:01:48 -0700

    agregando los componentes de tabulacion en el index para su facil importacion

--- libs/dimond-code-ui-lib/src/index.ts
+++ libs/dimond-code-ui-lib/src/index.ts
@@ -1,3 +1,7 @@
-export * from './lib/dimond-code.inputFa.module'
+export * from './lib/dmd-fa-input/dimond-code.inputFa.module'
 export * from './lib/dmd-fa-input/dmd-fa-input.component';
 export * from './lib/common/input-ref.directive';
+
+export * from './lib/tab-panel/tab-panel.module';
+export * from './lib/tab-panel/tab-panel.component';
+export * from './lib/tab-panel/tab-panel.component';
